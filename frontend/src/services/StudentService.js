f4129bfe5f6632809a403ec4651d1dd48190724f
SushmithaKatika <katikasushmitha7228@gmail.com>
2026-03-15 13:52:17 +0530

    Update API Endpoints

--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const REST_API_BASE_URL = 'http://localhost:8080/api/students';
+// const REST_API_BASE_URL = 'http://localhost:8080/api/students';
+const REST_API_BASE_URL = 'https://backend-production-1263.up.railway.app/api/students';
 
 export const listStudents = () => axios.get(REST_API_BASE_URL);
 
